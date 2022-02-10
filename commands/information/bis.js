7c970944cc79b83f61d40268fcff01f55a5ecc3a
Carson Mishoe <carson.legos@gmail.com>
2022-02-10 13:19:04 -0800

    improved the bis command to have very clean buttons

--- commands/information/bis.js
+++ commands/information/bis.js
@@ -1,29 +1,144 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageActionRow, MessageButton } = require('discord.js');
+const { MessageActionRow, MessageButton, CommandInteraction } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('bis')
-		.setDescription('Choose the job type for the best in slot you want'),
-	async execute(interaction, client) {
-		const row = new MessageActionRow()
-        .addComponents(
-        new MessageButton()
-            .setCustomId('dps-button')
-            .setLabel('DPS')
-            .setStyle("DANGER"),
-        new MessageButton()
-            .setCustomId("healer-button")
-            .setLabel('HEALER')
-            .setStyle("SUCCESS"),
-        new MessageButton()
-            .setCustomId("tank-button")
-            .setLabel("TANK")
-            .setStyle("PRIMARY"),
+		.setDescription('Choose the job type for the best in slot you want')
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName("ranged")
+				.setDescription("gets the buttons for ranged classes")
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName("melee")
+				.setDescription("gets the buttons for melee classes")
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName("magical")
+				.setDescription("gets the buttons for magical classes")
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName("healer")
+				.setDescription("gets the buttons for healer classes")
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName("tank")
+				.setDescription("gets the buttons for tank classes")
+		),
 
-            
-        );
+	async execute(interaction) {
+		if (interaction.options.getSubcommand() === "ranged") {
+			const row = new MessageActionRow()
+				.addComponents(
+					new MessageButton()
+						.setCustomId('machinist-button')
+						.setLabel('MCH')
+						.setStyle("DANGER"),
+					new MessageButton()
+						.setCustomId("dancer-button")
+						.setLabel('DNC')
+						.setStyle("DANGER"),
+					new MessageButton()
+						.setCustomId("bard-button")
+						.setLabel("BRD")
+						.setStyle("DANGER"),
+				)
+			await interaction.reply({ ephemeral: true, content: 'Which job?', components: [row] });
+		}
+		if (interaction.options.getSubcommand() === "melee") {
+			const row = new MessageActionRow()
+				.addComponents(
+					new MessageButton()
+						.setCustomId('samurai-button')
+						.setLabel('SAM')
+						.setStyle("DANGER"),
+					new MessageButton()
+						.setCustomId("dragoon-button")
+						.setLabel('DRG')
+						.setStyle("DANGER"),
+					new MessageButton()
+						.setCustomId("monk-button")
+						.setLabel("MNK")
+						.setStyle("DANGER"),
+					new MessageButton()
+						.setCustomId("ninja-button")
+						.setLabel("NIN")
+						.setStyle("DANGER"),
+					new MessageButton()
+						.setCustomId("reaper-button")
+						.setLabel("RPR")
+						.setStyle("DANGER"),
+				)
 
-        await interaction.reply({ ephemeral: true, content: 'Which Job Type?', components: [row]});
+			await interaction.reply({ ephemeral: true, content: 'Which job?', components: [row] });
+		}
+		if (interaction.options.getSubcommand() === "magical") {
+			const row = new MessageActionRow()
+				.addComponents(
+					new MessageButton()
+						.setCustomId('summoner-button')
+						.setLabel('MCH')
+						.setStyle("DANGER"),
+					new MessageButton()
+						.setCustomId("black-mage-button")
+						.setLabel('BLM')
+						.setStyle("DANGER"),
+					new MessageButton()
+						.setCustomId("red-mage-button")
+						.setLabel("RDM")
+						.setStyle("DANGER"),
+				)
+			await interaction.reply({ ephemeral: true, content: 'Which job?', components: [row] });
+		}
+		if (interaction.options.getSubcommand() === "healer") {
+			const row = new MessageActionRow()
+				.addComponents(
+					new MessageButton()
+						.setCustomId('sage-button')
+						.setLabel('SGE')
+						.setStyle('SUCCESS'),
+					new MessageButton()
+						.setCustomId("astrologian-button")
+						.setLabel('AST')
+						.setStyle("SUCCESS"),
+					new MessageButton()
+						.setCustomId("white-mage-button")
+						.setLabel("WHM")
+						.setStyle("SUCCESS"),
+					new MessageButton()
+						.setCustomId("scholar-button")
+						.setLabel("SCH")
+						.setStyle("SUCCESS"),
+				)
+			await interaction.reply({ ephemeral: true, content: 'Which job?', components: [row] });
+		}
+		if (interaction.options.getSubcommand() === "tank") {
+			const row = new MessageActionRow()
+				.addComponents(
+					new MessageButton()
+						.setCustomId('paladin-button')
+						.setLabel('PLD')
+						.setStyle("PRIMARY"),
+					new MessageButton()
+						.setCustomId("warrior-button")
+						.setLabel('WAR')
+						.setStyle("PRIMARY"),
+					new MessageButton()
+						.setCustomId("dark-knight-button")
+						.setLabel("DRK")
+						.setStyle("PRIMARY"),
+					new MessageButton()
+						.setCustomId("gunbreaker-button")
+						.setLabel("GNB")
+						.setStyle("PRIMARY"),
+				)
+			await interaction.reply({ ephemeral: true, content: 'Which job?', components: [row] });
+		}
+
 	},
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -8,19 +8,19 @@ module.exports = {
             if (!command) return;
 
             try {
-                await command.execute(interaction);
+                await command.execute(interaction, client);
             } catch (error) {
                 console.error(error);
                 await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
             }
         } else if (interaction.isButton()) {
             if (interaction.customId.includes('-button')) {
-                if (interaction.customId.includes('dps')) {
-                    await interaction.reply({ content: "Warrior"});
-                } else if (interaction.customId.includes('healer')) {
-                    await interaction.reply({ content: "Warrior"});
-                } else if (interaction.customId.includes('tank')) {
-                    await interaction.reply({ content: "Warrior"});
+                if (interaction.customId.includes('machinist')) {
+                    await interaction.reply({ ephemeral: true, content: "https://etro.gg/gearset/6b4b1ba5-a821-41a0-b070-b1f50e986f85"});
+                } else if (interaction.customId.includes('dancer')) {
+                    await interaction.reply({ ephemeral: true, content: "https://etro.gg/gearset/fd333e44-0f90-42a6-a070-044b332bb54e"});
+                } else if (interaction.customId.includes('bard')) {
+                    await interaction.reply({ ephemeral: true, content: "https://etro.gg/gearset/cec981af-25c7-4ffb-905e-3024411b797a"});
                 }
             }
         }
